553fffd2b7ec52e76a7a225d339783a75bdd2617
Zaid Qureshi <zaidqureshi@Zaids-Mac.local>
2020-01-26 07:21:41 -0800

    Project Strctured

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Text style={{alignSelf:'center'}}> this is the first project on ios </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
